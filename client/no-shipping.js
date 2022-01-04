4d594568cb95e7dcaab33df3324fea2c2a692e5e
andy <xxxandyfenxxx@xxx.com>
2022-01-04 15:53:01 -0800

    no shipping example

---
+++ client/no-shipping.js
@@ -0,0 +1,42 @@
+/* eslint-disable  no-alert, no-unused-vars */
+
+/*
+paypal
+  .Marks({
+    fundingSource: paypal.FUNDING.APPLEPAY,
+  })
+  .render("#applepay-mark");
+*/
+
+const order = {
+  purchase_units: [
+    {
+      payee: {
+        merchant_id: "XWVWZ4HG4YH9N",
+      },
+      amount: {
+        currency_code: "USD",
+        value: "7.05",
+      },
+    },
+  ],
+};
+
+
+paypal
+  .Buttons({
+    fundingSource: paypal.FUNDING.APPLEPAY,
+    style: {
+      label: "pay",
+      color: "black",
+    },
+    createOrder(data, actions) {
+      return actions.order.create(order);
+    },
+    onApprove(data, actions) {
+      return actions.order.capture().then(function (details) {
+        console.log(JSON.stringify(details, null, 4))
+      });
+    },
+  })
+  .render("#applepay-btn");
