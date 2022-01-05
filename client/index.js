e050cd2d02bcc0dbcf8e961225c1056d7946840e
andy <xxxandyfenxxx@xxx.com>
2022-01-05 14:56:17 -0800

    update

--- client/index.js
+++ client/index.js
@@ -1,74 +1,80 @@
 /* eslint-disable  no-alert, no-unused-vars */
 
+const amount = {
+  currency_code: "USD",
+  value: "7.05",
+  breakdown: {
+    item_total: {
+      currency_code: "USD",
+      value: "1.99",
+    },
+    tax_total: {
+      currency_code: "USD",
+      value: "0.07",
+    },
+    shipping: {
+      currency_code: "USD",
+      value: "4.99",
+    },
+  },
+};
+
+const shippingAddress = {
+  shipping_name: "Shipping To Cogny Cogny 69640",
+  phone: "143543778",
+  address_line_1: "33 Rue des Écoles",
+  address_line_2: "",
+  admin_area_1: "Cogny",
+  admin_area_2: "Cogny",
+  postal_code: "69640",
+  country_code: "FR",
+  address_details: {},
+};
+
+const shippingOptions = [
+  {
+    id: "1",
+    amount: {
+      currency_code: "USD",
+      value: "0.00",
+    },
+    type: "SHIPPING",
+    label: "Free Shipping",
+    selected: false,
+  },
+  {
+    id: "2",
+    amount: {
+      currency_code: "USD",
+      value: "9.99",
+    },
+    type: "SHIPPING",
+    label: "1-Day Shipping",
+    selected: false,
+  },
+  {
+    id: "3",
+    amount: {
+      currency_code: "USD",
+      value: "4.99",
+    },
+    type: "SHIPPING",
+    label: "3-Day Shipping",
+    selected: true,
+  },
+];
+
 const order = {
   purchase_units: [
     {
       payee: {
         merchant_id: "XWVWZ4HG4YH9N",
       },
-      amount: {
-        currency_code: "USD",
-        value: "7.05",
-        breakdown: {
-          item_total: {
-            currency_code: "USD",
-            value: "1.99",
-          },
-          tax_total: {
-            currency_code: "USD",
-            value: "0.07",
-          },
-          shipping: {
-            currency_code: "USD",
-            value: "4.99",
-          },
-        },
-      },
+      amount: amount,
       shipping: {
-        address: {
-          shipping_name: "Shipping To Cogny Cogny 69640",
-          phone: "143543778",
-          address_line_1: "33 Rue des Écoles",
-          address_line_2: "",
-          admin_area_1: "Cogny",
-          admin_area_2: "Cogny",
-          postal_code: "69640",
-          country_code: "FR",
-          address_details: {},
-        },
+        address: shippingAddress,
         method: "USPS",
-        options: [
-          {
-            id: "1",
-            amount: {
-              currency_code: "USD",
-              value: "0.00",
-            },
-            type: "SHIPPING",
-            label: "Free Shipping",
-            selected: false,
-          },
-          {
-            id: "2",
-            amount: {
-              currency_code: "USD",
-              value: "9.99",
-            },
-            type: "SHIPPING",
-            label: "1-Day Shipping",
-            selected: false,
-          },
-          {
-            id: "3",
-            amount: {
-              currency_code: "USD",
-              value: "4.99",
-            },
-            type: "SHIPPING",
-            label: "3-Day Shipping",
-            selected: true,
-          },
-        ],
+        options: shippingOptions,
       },
     },
   ],
@@ -93,21 +99,13 @@ paypal
     onShippingChange(data, actions) {
       const decimal = (strValue) => parseFloat(strValue, 10);
 
-      const {
-        amount,
-        orderID,
-        shipping_address,
-        selected_shipping_option,
-        facilitatorAccessToken,
-      } = data;
+      const { orderID, selected_shipping_option } = data;
 
       const {
-        amount: {
-          breakdown: { item_total, tax_total },
-        },
-      } = order.purchase_units[0]
+        breakdown: { item_total, tax_total },
+      } = amount;
 
-      console.log(JSON.stringify(data, null, 4));
+      console.log(JSON.stringify({ data, item_total, tax_total }, null, 4));
 
       let itemTotal = decimal(item_total.value);
       let taxAmount = decimal(tax_total.value);
