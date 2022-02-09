97529b0242296ae7f8734fc6dd5f0c6d4851eca9
andy <xxxandyfenxxx@xxx.com>
2022-02-09 12:29:08 -0800

    shipping update

--- examples/shipping-server.js
+++ examples/shipping-server.js
@@ -1,5 +1,79 @@
 /* eslint-disable  no-alert, no-unused-vars */
 
+const order2 = {
+  purchase_units: [{
+      payee: {
+        merchant_id: "XWVWZ4HG4YH9N",
+      },
+      amount: {
+          currency_code: 'USD',
+          value: '7.05',
+          breakdown: {
+              item_total: {
+                  currency_code: 'USD',
+                  value: '1.99'
+              },
+              tax_total: {
+                  currency_code: 'USD',
+                  value: '0.07'
+              },
+              shipping: {
+                  currency_code: 'USD',
+                  value: '4.99'
+              }
+          }
+      },
+      shipping: {
+          "address": {
+              "shipping_name": "Shipping To Cogny Cogny 69640",
+              "phone": "143543778",
+              "address_line_1": "33 Rue des Écoles",
+              "address_line_2": "",
+              "admin_area_1": "Cogny",
+              "admin_area_2": "Cogny",
+              "postal_code": "69640",
+              "country_code": "FR",
+              "address_details": {}
+          },
+          "method": "USPS",
+          options: [
+              {
+                  id: '1',
+                  amount: {
+                      currency_code: 'USD',
+                      value: '0.00'
+                  },
+                  type: 'SHIPPING',
+                  label: 'Free Shipping',
+                  selected: false
+              },
+              {
+                  id: '2',
+                  amount: {
+                      currency_code: 'USD',
+                      value: '9.99'
+                  },
+                  type: 'SHIPPING',
+                  label: '1-Day Shipping',
+                  selected: false
+              },
+              {
+                  id: '3',
+                  amount: {
+                      currency_code: 'USD',
+                      value: '4.99'
+                  },
+                  type: 'SHIPPING',
+                  label: '3-Day Shipping',
+                  selected: true
+              }
+          ]
+      }
+  }]
+}
+
+
+// 
 const shippingOptions = [
   {
     id: "1",
@@ -138,6 +212,7 @@ paypal
           return actions.resolve();
         })
         .catch((err) => {
+          console.log("err shiopping update")
           return actions.reject(err);
         });
     },
