d6de48c42bc5a29e68606069fa44c63750015297
andy <xxxandyfenxxx@xxx.com>
2022-02-09 11:34:53 -0800

    force https

--- server/index.js
+++ server/index.js
@@ -10,17 +10,18 @@ const { WEBHOOK_ID, PORT, PAYPAL_API_BASE } = require("./config");
 
 const app = express();
 
-app.use(express.static(resolve(__dirname, "../examples")));
-app.use(express.json());
-
-// requireHTTPS
+// require HTTPS
 app.use((req, res, next) => {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === "production") {
-    return res.redirect('https://' + req.get('host') + req.url);
+  if (req.get('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.get('host')}${req.url}`);
+  } else {
+    next();
   }
-  next();
 });
+
+app.use(express.static(resolve(__dirname, "../examples")));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "../examples/index.html"));
