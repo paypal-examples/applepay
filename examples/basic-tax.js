1abdd31e96dc9551ed5d924ec55c24c157582f76
andy <xxxandyfenxxx@xxx.com>
2022-02-10 13:47:03 -0800

    tax rate calc

--- examples/basic-tax.js
+++ examples/basic-tax.js
@@ -17,8 +17,8 @@ const order = {
           tax_total: {
             currency_code: "USD",
             value: "0.20",
-          }
-        }
+          },
+        },
       },
       shipping: {
         address: {
@@ -31,19 +31,32 @@ const order = {
           postal_code: "94107",
           country_code: "US",
           address_details: {},
-        }
-      }
+        },
+      },
+      method: "USPS",
+      options: [
+        {
+          id: "1",
+          amount: {
+            currency_code: "USD",
+            value: "0.00",
+          },
+          type: "SHIPPING",
+          label: "free Shipping",
+          selected: true,
+        },
+      ],
     },
-  ]
+  ],
 };
 
-async function caculateShipping(shippingAddress){
-  console.log("Tax update for postcode %s", shippingAddress.postal_code)
+async function caculateShipping(shippingAddress) {
+  console.log("Tax update for postcode %s", shippingAddress.postal_code);
 
   return {
     // random sales tax rate 0 - 10%
-    taxRate: (Math.random() * 10).toFixed(2)
-  }
+    taxRate: (Math.random() * 10).toFixed(2),
+  };
 }
 
 paypal
@@ -57,61 +70,63 @@ paypal
       return actions.order.create(order);
     },
     onApprove(data, actions) {
-      console.log("Order approved")
+      console.log("Order approved");
 
       fetch(`/capture/${data.orderID}`, {
         method: "post",
       })
         .then((res) => res.json())
         .then((data) => {
-          alert("order captured")
+          alert("order captured");
         })
         .catch(console.error);
     },
     onShippingChange(data, actions) {
-      console.log("onShippingChange");
-      console.log(JSON.stringify(data, null, 4));
-
-      return fetch(`/orders/${data.orderID}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify([
-          {
-            op: "replace",
-            path: "/purchase_units/@reference_id=='default'/amount",
-            value: {
-              currency_code: "USD",
-              value: "1.30",
-              breakdown: {
-                item_total: {
-                  currency_code: "USD",
-                  value: "1.00",
-                },
-                tax_total: {
-                  currency_code: "USD",
-                  value: "0.30",
-                }
-              }
+      caculateShipping(data.shipping_address)
+        .then(({ taxRate }) => {
+          fetch(`/orders/${data.orderID}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
             },
-          },
-        ]),
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("patching order");
-          }
-          return res.json();
+            body: JSON.stringify([
+              {
+                op: "replace",
+                path: "/purchase_units/@reference_id=='default'/amount",
+                value: {
+                  currency_code: "USD",
+                  value: "1.30",
+                  breakdown: {
+                    item_total: {
+                      currency_code: "USD",
+                      value: "1.00",
+                    },
+                    tax_total: {
+                      currency_code: "USD",
+                      value: "0.30",
+                    },
+                  },
+                },
+              },
+            ]),
+          })
+            .then((res) => {
+              if (!res.ok) {
+                throw new Error("patching order");
+              }
+              return res.json();
+            })
+            .then((json) => {
+              console.log(
+                `Successful Order patch call: ${JSON.stringify(json)}`
+              );
+              return actions.resolve();
+            })
+            .catch((err) => {
+              return actions.reject(err);
+            });
         })
-        .then((json) => {
-          console.log(`Successful Order patch call: ${JSON.stringify(json)}`);
-          return actions.resolve();
-        })
-        .catch((err) => {
-          return actions.reject(err);
-        });
-
-    }
+        .catch(console.error);
+    },
   })
   .render("#applepay-btn");
