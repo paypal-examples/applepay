5a5dccdc6c5e23cd00141b46452fd0361fc7c732
andy <xxxandyfenxxx@xxx.com>
2022-02-24 13:40:03 -0800

    renamee

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const { getAccessToken } = require("./paypal");
-const { WEBHOOK_ID, PORT, PAYPAL_API_BASE } = require("./config");
+const { PORT, PAYPAL_API_BASE } = require("./config");
 const { requireHTTPS } = require("./middleware");
 
 const app = express();
@@ -131,72 +131,6 @@ app.post("/calculate-shipping", (req, res) => {
     updatedShippingOptions,
     isShippingTaxable,
   });
-});
-
-/**
- * Webhook handlers.
- */
-app.post("/webhook", async (req, res) => {
-  const { access_token } = await getAccessToken();
-
-  const { event_type, resource } = req.body;
-  const orderId = resource.id;
-
-  console.log(`🪝 Recieved Webhook Event`);
-
-  /* verify the webhook signature */
-  try {
-    const { data } = await axios({
-      url: `${PAYPAL_API_BASE}/v1/notifications/verify-webhook-signature`,
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: `Bearer ${access_token}`,
-      },
-      data: {
-        transmission_id: req.headers["paypal-transmission-id"],
-        transmission_time: req.headers["paypal-transmission-time"],
-        cert_url: req.headers["paypal-cert-url"],
-        auth_algo: req.headers["paypal-auth-algo"],
-        transmission_sig: req.headers["paypal-transmission-sig"],
-        webhook_id: WEBHOOK_ID,
-        webhook_event: req.body,
-      },
-    });
-
-    const { verification_status } = data;
-
-    if (verification_status !== "SUCCESS") {
-      console.log(`⚠️  Webhook signature verification failed.`);
-      return res.sendStatus(400);
-    }
-  } catch (err) {
-    console.log(`⚠️  Webhook signature verification failed.`);
-    return res.sendStatus(400);
-  }
-
-  /* capture the order */
-  if (event_type === "CHECKOUT.ORDER.APPROVED") {
-    try {
-      await axios({
-        url: `${PAYPAL_API_BASE}/v2/checkout/orders/${orderId}/capture`,
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${access_token}`,
-        },
-      });
-
-      console.log(`💰 Payment captured!`);
-    } catch (err) {
-      console.log(`❌ Payment failed.`);
-      return res.sendStatus(400);
-    }
-  }
-
-  res.sendStatus(200);
 });
 
 app.listen(PORT, async () => {
