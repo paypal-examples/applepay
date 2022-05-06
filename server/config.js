90369ea59aa2c430b4eccbd4485ac76ae3386dcc
andy <xxxandyfenxxx@xxx.com>
2022-05-06 12:36:03 -0700

    prod api url

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const { NODE_ENV, CLIENT_ID, CLIENT_SECRET } = process.env;
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  PAYPAL_API_BASE: process.env.PAYPAL_API_BASE || "https://api.sandbox.paypal.com",
+  PAYPAL_API_BASE: process.env.PAYPAL_API_BASE || "https://api.paypal.com", //  "https://api.sandbox.paypal.com",
   NODE_ENV, CLIENT_ID, CLIENT_SECRET,
   DISABLE_CAPTURE: process.env.DISABLE_CAPTURE === "true"
 };
