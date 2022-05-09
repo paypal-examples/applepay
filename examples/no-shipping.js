87b000d9fd964c44bfb67e5a50cfcf78e284be3f
andy <xxxandyfenxxx@xxx.com>
2022-05-09 09:17:57 -0700

    no shipping

---
+++ examples/no-shipping.js
@@ -0,0 +1,40 @@
+/* eslint-disable  no-alert, no-unused-vars */
+
+const order = {
+  purchase_units: [
+    {
+      amount: {
+        currency_code: "USD",
+        value: "0.02"
+      }
+    },
+  ],
+  application_context: {
+    shipping_preference: "NO_SHIPPING"
+  }
+};
+
+paypal
+  .Buttons({
+    fundingSource: paypal.FUNDING.APPLEPAY,
+    style: {
+      label: "pay",
+      color: "black",
+    },
+    createOrder(data, actions) {
+      return actions.order.create(order);
+    },
+    onApprove(data, actions) {
+      console.log("Order approved")
+
+      fetch(`/capture/${data.orderID}`, {
+        method: "post",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("order captured")
+        })
+        .catch(console.error);
+    },
+  })
+  .render("#applepay-btn");
