b5fef006718c8056628470f9b4afea68075f7607
cnallam <cnallam@paypal.com>
2023-11-07 16:37:46 -0800

    Modified heathcheck api

--- advanced-integration/server.js
+++ advanced-integration/server.js
@@ -44,9 +44,6 @@ app.get("/check", (req, res) => {
   res.json({
     message: "ok",
     env: process.env.NODE_ENV,
-    clientId: process.env.PAYPAL_CLIENT_ID,
-    appSecret: process.env.PAYPAL_CLIENT_SECRET || "Couldn't load App Secret",
-    clientSecret: process.env.PAYPAL_CLIENT_SECRET,
     merchantId: process.env.PAYPAL_MERCHANT_ID
   })
 })
